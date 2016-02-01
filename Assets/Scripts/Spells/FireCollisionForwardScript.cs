82ba94b9e955411661858c7a3953ee6e560ffb0c
yroquain <yroquain@gmail.com>
2016-02-01 12:44:29 -0500

    fix trait de feu

--- Assets/Scripts/Spells/FireCollisionForwardScript.cs
+++ Assets/Scripts/Spells/FireCollisionForwardScript.cs
@@ -7,19 +7,19 @@ using System.Collections;
         void HandleCollision(GameObject obj, Collision c);
     }
 
-    /// <summary>
-    /// This script simply allows forwarding collision events for the objects that collide with something. This
-    /// allows you to have a generic collision handler and attach a collision forwarder to your child objects.
-    /// In addition, you also get access to the game object that is colliding, along with the object being
-    /// collided into, which is helpful.
-    /// </summary>
-    public class FireCollisionForwardScript : MonoBehaviour
-    {
-        public ICollisionHandler CollisionHandler;
+/// <summary>
+/// This script simply allows forwarding collision events for the objects that collide with something. This
+/// allows you to have a generic collision handler and attach a collision forwarder to your child objects.
+/// In addition, you also get access to the game object that is colliding, along with the object being
+/// collided into, which is helpful.
+/// </summary>
+public class FireCollisionForwardScript : MonoBehaviour
+{
+    public ICollisionHandler CollisionHandler;
 
-        public void OnCollisionEnter(Collision col)
-        {
-            CollisionHandler.HandleCollision(gameObject, col);
-        }
+    public void OnCollisionEnter(Collision col)
+    {
+        CollisionHandler.HandleCollision(gameObject, col);
     }
+}
 
--- Assets/Scripts/Spells/FireProjectileScript.cs
+++ Assets/Scripts/Spells/FireProjectileScript.cs
@@ -9,99 +9,99 @@ using System.Collections;
     /// <param name="pos">Position</param>
     public delegate void FireProjectileCollisionDelegate(FireProjectileScript script, Vector3 pos);
 
-    /// <summary>
-    /// This script handles a projectile such as a fire ball
-    /// </summary>
-    public class FireProjectileScript : FireBaseScript, ICollisionHandler
+/// <summary>
+/// This script handles a projectile such as a fire ball
+/// </summary>
+public class FireProjectileScript : FireBaseScript, ICollisionHandler
+{
+    [Tooltip("The collider object to use for collision and physics.")]
+    public GameObject ProjectileColliderObject;
+
+    [Tooltip("The sound to play upon collision.")]
+    public AudioSource ProjectileCollisionSound;
+
+    [Tooltip("The particle system to play upon collision.")]
+    public ParticleSystem ProjectileExplosionParticleSystem;
+
+    [Tooltip("The radius of the explosion upon collision.")]
+    public float ProjectileExplosionRadius = 50.0f;
+
+    [Tooltip("The force of the explosion upon collision.")]
+    public float ProjectileExplosionForce = 50.0f;
+
+    [Tooltip("An optional delay before the collider is sent off, in case the effect has a pre fire animation.")]
+    public float ProjectileColliderDelay = 0.0f;
+
+    [Tooltip("The speed of the collider.")]
+    public float ProjectileColliderSpeed = 450.0f;
+
+    [Tooltip("The direction that the collider will go. For example, flame strike goes down, and fireball goes forward.")]
+    public Vector3 ProjectileDirection = Vector3.forward;
+
+    [Tooltip("What layers the collider can collide with.")]
+    public LayerMask ProjectileCollisionLayers = Physics.AllLayers;
+
+    [Tooltip("Particle systems to destroy upon collision.")]
+    public ParticleSystem[] ProjectileDestroyParticleSystemsOnCollision;
+
+    [HideInInspector]
+    public FireProjectileCollisionDelegate CollisionDelegate;
+
+    private bool collided;
+
+    private IEnumerator SendCollisionAfterDelay()
     {
-        [Tooltip("The collider object to use for collision and physics.")]
-        public GameObject ProjectileColliderObject;
+        yield return new WaitForSeconds(ProjectileColliderDelay);
 
-        [Tooltip("The sound to play upon collision.")]
-        public AudioSource ProjectileCollisionSound;
+        Vector3 dir = ProjectileDirection * ProjectileColliderSpeed;
+        dir = ProjectileColliderObject.transform.rotation * dir;
+        ProjectileColliderObject.GetComponent<Rigidbody>().velocity = dir;
+    }
 
-        [Tooltip("The particle system to play upon collision.")]
-        public ParticleSystem ProjectileExplosionParticleSystem;
+    protected override void Start()
+    {
+        base.Start();
 
-        [Tooltip("The radius of the explosion upon collision.")]
-        public float ProjectileExplosionRadius = 50.0f;
+        StartCoroutine(SendCollisionAfterDelay());
+    }
 
-        [Tooltip("The force of the explosion upon collision.")]
-        public float ProjectileExplosionForce = 50.0f;
-
-        [Tooltip("An optional delay before the collider is sent off, in case the effect has a pre fire animation.")]
-        public float ProjectileColliderDelay = 0.0f;
-
-        [Tooltip("The speed of the collider.")]
-        public float ProjectileColliderSpeed = 450.0f;
-
-        [Tooltip("The direction that the collider will go. For example, flame strike goes down, and fireball goes forward.")]
-        public Vector3 ProjectileDirection = Vector3.forward;
-
-        [Tooltip("What layers the collider can collide with.")]
-        public LayerMask ProjectileCollisionLayers = Physics.AllLayers;
-
-        [Tooltip("Particle systems to destroy upon collision.")]
-        public ParticleSystem[] ProjectileDestroyParticleSystemsOnCollision;
-
-        [HideInInspector]
-        public FireProjectileCollisionDelegate CollisionDelegate;
-
-        private bool collided;
-
-        private IEnumerator SendCollisionAfterDelay()
+    public void HandleCollision(GameObject obj, Collision c)
+    {
+        if (collided)
         {
-            yield return new WaitForSeconds(ProjectileColliderDelay);
-
-            Vector3 dir = ProjectileDirection * ProjectileColliderSpeed;
-            dir = ProjectileColliderObject.transform.rotation * dir;
-            ProjectileColliderObject.GetComponent<Rigidbody>().velocity = dir;
+            // already collided, don't do anything
+            return;
         }
 
-        protected override void Start()
-        {
-            base.Start();
+        // stop the projectile
+        collided = true;
+        Stop();
 
-            StartCoroutine(SendCollisionAfterDelay());
+        // destroy particle systems after a slight delay
+        if (ProjectileDestroyParticleSystemsOnCollision != null)
+        {
+            foreach (ParticleSystem p in ProjectileDestroyParticleSystemsOnCollision)
+            {
+                GameObject.Destroy(p, 0.1f);
+            }
         }
 
-        public void HandleCollision(GameObject obj, Collision c)
+        // play collision sound
+        if (ProjectileCollisionSound != null)
         {
-            if (collided)
-            {
-                // already collided, don't do anything
-                return;
-            }
+            ProjectileCollisionSound.Play();
+        }
 
-            // stop the projectile
-            collided = true;
-            Stop();
-
-            // destroy particle systems after a slight delay
-            if (ProjectileDestroyParticleSystemsOnCollision != null)
+        // if we have contacts, play the collision particle system and call the delegate
+        if (c.contacts.Length != 0)
+        {
+            ProjectileExplosionParticleSystem.transform.position = c.contacts[0].point;
+            ProjectileExplosionParticleSystem.Play();
+            FireBaseScript.CreateExplosion(c.contacts[0].point, ProjectileExplosionRadius, ProjectileExplosionForce);
+            if (CollisionDelegate != null)
             {
-                foreach (ParticleSystem p in ProjectileDestroyParticleSystemsOnCollision)
-                {
-                    GameObject.Destroy(p, 0.1f);
-                }
-            }
-
-            // play collision sound
-            if (ProjectileCollisionSound != null)
-            {
-                ProjectileCollisionSound.Play();
-            }
-
-            // if we have contacts, play the collision particle system and call the delegate
-            if (c.contacts.Length != 0)
-            {
-                ProjectileExplosionParticleSystem.transform.position = c.contacts[0].point;
-                ProjectileExplosionParticleSystem.Play();
-                FireBaseScript.CreateExplosion(c.contacts[0].point, ProjectileExplosionRadius, ProjectileExplosionForce);
-                if (CollisionDelegate != null)
-                {
-                    CollisionDelegate(this, c.contacts[0].point);
-                }
+                CollisionDelegate(this, c.contacts[0].point);
             }
         }
     }
+}
