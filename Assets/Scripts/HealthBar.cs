a557a70fad7d1613fe0367506d7e113a96f6829f
Thibaut <thibaut.desmarez@isen-lille.fr>
2016-02-01 15:18:48 -0500

    ajout barre de vie + auto regen sur le temps

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState { playing, gameover, pause };
+
+
+public class HealthBar : MonoBehaviour
+{
+
+    public static HealthBar SP;
+    public static int score;
+
+    private int bestScore = 0;
+    public static GameState gameState;
+
+    float curHP = 100.0f;
+    float maxHP = 100.0f;
+    public GUISkin myskin;
+    public Texture2D HpBarTexture;
+    public float HpBarLenght;
+    public int HpBarX = 160;
+    public int HpBarY = 10;
+    public int HpBarHeight = 15;
+    float PercentOfHP;
+
+    void Awake()
+    {
+        SP = this;
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Time.timeScale = 1.0f; //met vitesse du jeu à 1
+        gameState = GameState.playing;
+    }
+
+
+
+    void OnGUI()
+    {
+        GUI.skin = myskin;
+        GUILayout.Space(3);
+
+        if (curHP > 0)
+        {
+            GUI.DrawTexture(new Rect(HpBarX, HpBarY, HpBarLenght, HpBarHeight), HpBarTexture);
+        }
+
+        if (gameState == GameState.gameover)
+        {
+            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+
+            GUILayout.Label("Game over!");
+
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
+
+        }
+    }
+
+    public float getCurHP()
+    {
+        return this.curHP;
+    }
+
+    public void setCurHP(float curHP)
+    {
+        this.curHP = curHP;
+    }
+
+
+    void GameOver()
+    {
+        Time.timeScale = 0.0f; //Pause the game
+        gameState = GameState.gameover;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0.0f; //Pause the game
+    }
+
+    void Update()
+    {
+
+        //auto regen
+        curHP += Time.deltaTime * 5;
+
+
+        if (curHP > maxHP)
+        {
+            curHP = maxHP;
+        }
+
+        PercentOfHP = curHP / maxHP;
+        HpBarLenght = PercentOfHP * 130;
+
+        //if hp = 0
+        if (HealthBar.SP.getCurHP() <= 0)
+        {
+            this.GameOver();
+        }
+
+        /*
+        if (Input.GetKeyDown("y"))
+        {
+            HealthBar.SP.setCurHP(HealthBar.SP.getCurHP() - 15.0f);
+            Debug.Log("retire 15 hp");
+    }
+    */    
+
+
+
+
+
+    }
+}
