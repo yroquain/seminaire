b9aec16130c68efd8dd461a7e6334902949f1e93
yroquain <yroquain@gmail.com>
2016-02-02 14:50:30 -0500

    make it rain v3

---
+++ Assets/Scripts/Spells/TimeRain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeRain : MonoBehaviour {
+
+
+    private float timebeforedeath;
+	// Use this for initialization
+	void Start () {
+        timebeforedeath = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Time.time> timebeforedeath+7)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
