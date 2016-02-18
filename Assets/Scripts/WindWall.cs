5b16bb65ff40458787983057dbbf9b87c2f7975f
yroquain <yroquain@gmail.com>
2016-02-18 01:33:40 -0500

    barriere givree

--- Assets/Scripts/WindWall.cs
+++ Assets/Scripts/WindWall.cs
@@ -5,10 +5,14 @@ public class WindWall : MonoBehaviour {
 
     public GameObject particle2;
     public GameObject particle1;
+    public GameObject murG;
+    private bool IsIfrit;
+    private bool IsGivre;
     // Use this for initialization
     void Start () {
-	
-	}
+        IsIfrit = false;
+        IsGivre = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,13 +21,24 @@ public class WindWall : MonoBehaviour {
 
     void OnTriggerEnter(Collider coll)
     {
-        if(coll.tag=="Mage_Feu")
+        if (!IsIfrit && !IsGivre)
         {
-            if (coll.GetComponent<PlayerController>().IsImmolating)
+            if (coll.tag == "Mage_Feu")
             {
-                particle1.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
-                particle2.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
-                this.tag = "MurIfrit";
+                if (coll.GetComponent<PlayerController>().IsImmolating)
+                {
+                    particle1.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
+                    particle2.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
+                    this.tag = "MurIfrit";
+                    IsIfrit = true;
+                }
+            }
+            if (coll.tag == "ChocAquatique")
+            {
+                particle1.SetActive(false);
+                murG.SetActive(true);
+                IsGivre = true;
+                this.tag = "BarriereGivree";
             }
         }
     }
