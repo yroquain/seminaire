5a4ef6b096466606c28b3a31ff55324858694322
yroquain <yroquain@gmail.com>
2016-02-19 16:12:18 -0500

    windwall+salle boss

--- Assets/Scripts/WindWall.cs
+++ Assets/Scripts/WindWall.cs
@@ -27,14 +27,24 @@ public class WindWall : MonoBehaviour {
             {
                 if (coll.GetComponent<PlayerController>().IsImmolating)
                 {
-                    particle1.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
-                    particle2.GetComponent<ParticleSystem>().startColor = new Color(1, 0, 0, .08f);
+                    particle1.GetComponent<ParticleSystem>().startColor = new Color(1, 0.5f, 0, 0f);
+                    particle2.GetComponent<ParticleSystem>().startColor = new Color(1, 0.25f, 0, .5f);
+                    particle2.GetComponent<ParticleSystem>().startLifetime = 4;
+                    /*var overtime1 = particle1.GetComponent<ParticleSystem>().colorOverLifetime;
+                    overtime1.enabled = true;
+                    Gradient grad = new Gradient();
+                    grad.SetKeys(new GradientColorKey[] { new GradientColorKey(new Color(1,0.5f,0,1), 0.0f), new GradientColorKey(Color.red, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+                    overtime1.color= new ParticleSystem.MinMaxGradient(grad);
+                    var overtime2 = particle2.GetComponent<ParticleSystem>().colorOverLifetime;
+                    overtime2.enabled = true;
+                    overtime2.color = new ParticleSystem.MinMaxGradient(grad);*/
                     this.tag = "MurIfrit";
                     IsIfrit = true;
                 }
             }
             if (coll.tag == "ChocAquatique")
             {
+                GetComponent<Collider>().enabled = false;
                 particle1.SetActive(false);
                 murG.SetActive(true);
                 IsGivre = true;
