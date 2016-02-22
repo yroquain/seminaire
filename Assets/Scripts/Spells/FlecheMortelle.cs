dfa498e1b27cd3a392435f2354dff9164aac16b8
yroquain <yroquain@gmail.com>
2016-02-22 11:17:12 -0500

    fleche mortelle

---
+++ Assets/Scripts/Spells/FlecheMortelle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlecheMortelle : MonoBehaviour
+{
+
+    private float todie;
+    // Use this for initialization
+    void Start()
+    {
+        todie = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > todie + 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.gameObject.tag != "Mage_Feu" && Coll.gameObject.tag != "Mage_Eau" && Coll.gameObject.tag != "Mage_Air" && Coll.gameObject.name != "FireboltCollider" && Coll.gameObject.name != "Trigger1C" && Coll.gameObject.name != "Trigger2C" && Coll.gameObject.tag!="MurEole")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Spells/TraitFeu.cs
+++ Assets/Scripts/Spells/TraitFeu.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class TraitFeu : MonoBehaviour {
     private GameObject cameraa;
+    public GameObject fleche;
     private float todie;
     // Use this for initialization
     void Start () {
@@ -25,6 +26,11 @@ public class TraitFeu : MonoBehaviour {
     {
         if (Coll.gameObject.tag != "Mage_Feu" && Coll.gameObject.tag != "Mage_Eau" && Coll.gameObject.tag != "Mage_Air" && Coll.gameObject.name!="FireboltCollider" && Coll.gameObject.name !="Trigger1C" && Coll.gameObject.name !="Trigger2C")
         {
+            if(Coll.tag=="MurEole")
+            {
+                GameObject FlecheMortelle = (GameObject)Instantiate(fleche, this.transform.position, Quaternion.Euler(cameraa.transform.rotation.x / 3.14f * 360+270, cameraa.transform.rotation.y / 3.14f * 360, cameraa.transform.rotation.z / 3.14f * 360));
+                FlecheMortelle.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
+            }
             Destroy(GameObject.Find("Firebolt(Clone)"));
             Destroy(gameObject);
         }
