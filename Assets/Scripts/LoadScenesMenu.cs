14a67057ed81d054c018403abeefae78b47334b9
Thibaut <thibaut.desmarez@isen-lille.fr>
2016-02-25 18:20:18 -0500

    ajout sur le menu

---
+++ Assets/Scripts/LoadScenesMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScenesMenu : MonoBehaviour
+{
+    public void loadNewGame()
+    {
+        SceneManager.LoadScene(1);
+       // Application.LoadLevel(0);
+    }
+
+    public void loadTutorial()
+    {
+
+    }
+    public void loadOptions()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("tototo");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/positionMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class positionMenu : MonoBehaviour {
+
+    private float widthScreen;
+    public float x;
+    public float y;
+    // Use this for initialization
+    void Update () {
+        widthScreen = Screen.width;
+        Debug.Log(widthScreen);
+        GameObject.Find("Nouvelle Partie").GetComponent<RectTransform>().position = new Vector3(0.48f * widthScreen, 0.37f * widthScreen, 0);
+        GameObject.Find("Tutoriel").GetComponent<RectTransform>().position = new Vector3(0.5f * widthScreen, 0.31f * widthScreen, 0);
+        GameObject.Find("Options").GetComponent<RectTransform>().position = new Vector3(0.5f * widthScreen, 0.25f * widthScreen, 0);
+        GameObject.Find("Quitter Jeu").GetComponent<RectTransform>().position = new Vector3(0.5f * widthScreen, 0.19f * widthScreen, 0);
+
+    }
+}
